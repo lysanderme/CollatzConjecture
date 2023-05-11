b6547fe37a9906ff8f25dff2d3ec93a2aeb3d870
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-05-11 14:10:10 +0100

    Initial Commit

---
+++ CollatzConjecture/CollatzConjectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollatzConjectureApp.swift
+//  CollatzConjecture
+//
+//  Created by Lysander Myrddin-Evans on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CollatzConjectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
